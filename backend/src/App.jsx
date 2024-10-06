412cebcdb793e980c37d41f7045b065ce3048d58
Aaron <correaaaron70@gmail.com>
2024-10-06 23:45:02 +0530

    Installed shadcn

---
+++ backend/src/App.jsx
@@ -0,0 +1,13 @@
+import SiteHeader from "@/components/siteHeader";
+
+function App() {
+  return (
+    <>
+      <div className="flex min-h-screen flex-col">
+        <SiteHeader />
+      </div>
+    </>
+  );
+}
+
+export default App;
